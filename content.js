b5fc0eb3dea957ee6f762c0cd6a1269984fc1ba4
michelle54725 <michelle54725@gmail.com>
2020-04-07 17:30:39 +0800

    initial commit

---
+++ content.js
@@ -0,0 +1,35 @@
+const interactive = 
+[
+	{
+		"type": "section",
+		"text": {
+			"type": "mrkdwn",
+			"text": "Find out more about AWE:\n*<https://awe.berkeley.edu/|AWE's Website>*"
+		}
+	},
+	{
+		"type": "actions",
+		"elements": [
+			{
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"emoji": true,
+					"text": "Approve"
+				},
+				"style": "primary",
+				"value": "click_me_123"
+			},
+			{
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"emoji": true,
+					"text": "Deny"
+				},
+				"style": "danger",
+				"value": "click_me_123"
+			}
+		]
+	}
+]
